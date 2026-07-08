cd16feb7e0b65b9658625884b8e7e81674c6abdc
neverland <chenjiahan.jait@bytedance.com>
2026-07-08 20:57:24 +0800

    chore: use Rstack CLI

--- rstest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@rstest/core';
-import { withRslibConfig } from '@rstest/adapter-rslib';
-
-export default defineConfig({
-  extends: withRslibConfig(),
-  include: ['tests/**/*.test.ts'],
-  testEnvironment: 'node',
-});
